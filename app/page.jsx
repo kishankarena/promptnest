cc9185ce79e4591c9fd877d86216f5e37522b670
kishankarena <kishan.k@simformsolutions.com>
2024-07-17 16:30:10 +0530

    feature/navigation bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -12,6 +14,7 @@ const Home = () => {
         and disseminate imaginative prompts, harnessing the power of AI to
         inspire new ideas.
       </p>
+      <Feed />
     </section>
   );
 };
