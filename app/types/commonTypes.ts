e69afe1b684c9b8fe0ad533103b7dd9412f8224b
kishankarena <kishan.k@simformsolutions.com>
2024-09-12 11:53:22 +0530

    feature : add feature page to display prompts

--- app/types/commonTypes.ts
+++ app/types/commonTypes.ts
@@ -1,5 +1,20 @@
 export interface UserType {
   id: string;
+  username?: string;
   email: string;
   image: string;
 }
+
+interface Creator {
+  _id: string;
+  email: string;
+  username: string;
+  image: string;
+}
+export interface Prompt {
+  _id: string;
+  prompt: string;
+  tag: string;
+  creator?: Creator;
+}
+
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+import { Prompt } from "@/app/types/commonTypes";
+
+interface PrompoCardListProps {
+  data: Prompt[];
+  handleTagClick: (tag: string) => void;
+}
+
+const PromptCardList: React.FC<PrompoCardListProps> = ({
+  data,
+  handleTagClick,
+}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post: Prompt) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed: React.FC = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState<string>("");
+  const [allPosts, setAllPosts] = useState<Prompt[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setAllPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e: React.FormEvent) => {};
+  const handleTagClick = () => {};
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input"
+        />
+      </form>
+      <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+    </section>
+  );
 };
 
 export default Feed;
